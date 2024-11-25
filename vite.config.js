ea5c822cae36595386fd5a09fb5f19155a0d6243
lyfe691 <yanis.sebastian.zuercher@gmail.com>
2024-11-25 21:15:57 +0100

    gh configs for custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,21 @@
-import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "::",
-    port: "8080",
-  },
   plugins: [react()],
   resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: fileURLToPath(new URL("./src", import.meta.url)),
-      },
-      {
-        find: "lib",
-        replacement: resolve(__dirname, "lib"),
-      },
-    ],
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)), 
+    },
+  },
+  server: {
+    host: "::", 
+    port: 8080,
+  },
+  build: {
+    outDir: "dist", 
+    emptyOutDir: true, 
   },
 });
