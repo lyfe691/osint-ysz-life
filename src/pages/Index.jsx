86efeda113d4242baf97e44fa1e165363d910314
lyfe691 <yanis.sebastian.zuercher@gmail.com>
2024-11-25 17:08:29 +0100

    small changes

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -31,7 +31,7 @@ const Index = () => {
       <div className="container py-12">
         <header className="text-center mb-16">
           <h1 className="text-4xl font-bold mb-4">OSINT Excercises</h1>
-          <p className="text-xl text-gray-400">These are some excersises I put together for you to completely for free!</p>
+          <p className="text-xl text-gray-400">These are some excersises I put together for you completely for free!</p>
         </header>
 
         <div className="mb-8">
